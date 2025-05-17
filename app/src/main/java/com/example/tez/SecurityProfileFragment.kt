c6071eb77760f680375fc09b668ea957b49d7dd2
Nehir Ünal <nehiru@icloud.com>
2025-05-17 12:15:53 +0300

    nehir10:  UI değişiklikleri

--- app/src/main/java/com/example/tez/SecurityProfileFragment.kt
+++ app/src/main/java/com/example/tez/SecurityProfileFragment.kt
@@ -5,19 +5,33 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.tez.databinding.FragmentChangePasswordBinding
+import com.example.tez.databinding.FragmentSecurityProfileBinding
 
 
 class SecurityProfileFragment : Fragment() {
 
 
 
+    private var _binding: FragmentSecurityProfileBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_security_profile, container, false)
+    ): View {
+        _binding = FragmentSecurityProfileBinding.inflate(inflater, container, false)
+        return binding.root
+    }
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        binding.leftSecurity.setOnClickListener() {
+            findNavController().navigate(R.id.action_securityProfileFragment_to_profileFragment)
+        }
     }
 
 
